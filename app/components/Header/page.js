574a38dbfb536659eb25017bf225ce2fc098b634
Arghya20 <arghyaghosh.vlog@gmail.com>
2023-09-23 01:27:37 +0530

    complete header part

--- app/components/Header/page.js
+++ app/components/Header/page.js
@@ -1,6 +1,10 @@
 const Header = () => {
   return (
-    <div className="sm:max-w-xl md:max-w-full lg:max-w-screen-xl mx-auto"></div>
+    <div className="sm:max-w-xl md:max-w-full lg:max-w-screen-xl mx-auto flex justify-center items-center cursor-pointer text-4xl font-bold uppercase pt-12 tracking-[12px]">
+      <span className="bg-clip-text text-transparent bg-gradient-to-r from-start via-middle to-end">
+        DEVELOPS
+      </span>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        start: "#69eacb",
+        middle: "#eaccf8",
+        end: "#6755f1",
+      },
     },
   },
   plugins: [],
